2f7983c483c7da9fc67c2defb22898871a5f08ea
Ayomide Akintimehin <ayoakinti@gmail.com>
2025-05-06 08:47:20 +0200

    redundant files deleted and basic SEO added

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { APIError } from '../classes/error.class'
+import { APIError } from '@/classes/error.class'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3001',
